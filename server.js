052d3ae5f828851ab4925e82f6c58cd721df9f8d
Matt B <mattjballard81@gmail.com>
2020-09-18 11:57:35 -0400

    set up basic user route validation

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect to database
 connectDB();
 
+//init middleware to use body parser
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Howdy!"));
 
 // define routes
