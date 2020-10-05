a0ab7ea9d45df42d154250a8ea96e8a9ca105ec3
Matt B <mattjballard81@gmail.com>
2020-10-05 12:39:35 -0400

    Add UI functionality to add, update and delete contacts

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,84 @@
+import React, { useReducer } from "react";
+import uuid from "uuid/dist/v4";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER } from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Samuel Samson",
+        email: "samsam@gmail.com",
+        phone: "123-456-7890",
+        type: "professional",
+      },
+      {
+        id: 2,
+        name: "John Johnson",
+        email: "johnjohn@yahoo.com",
+        phone: "555-555-5555",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Bob Loblaw",
+        email: "bobloblaw@lawblog.com",
+        phone: "519-555-5519",
+        type: "professional",
+      },
+    ],
+    current: null,
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // add contact
+  const addContact = contact => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+
+  // delete contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  // set current contact
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
+  // clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  // update contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
+
+  // filter contacts
+
+  // clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
